56fa9ae39e2f7ffa86bfc409dfd27a82fe226112
castangel <castelain.angel@gmail.com>
2024-02-01 10:34:03 +0100

    console creation

---
+++ EasySave/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine(" ______                                    ");
+Console.WriteLine("|  ____|                                   ");
+Console.WriteLine("| |__   __ _ ___ _   _ ___  __ ___   _____ ");
+Console.WriteLine("|  __| / _` / __| | | / __|/ _` \\ \\ / / _ \\");
+Console.WriteLine("| |___| (_| \\__ \\ |_| \\__ \\ (_| |\\ V /  __/");
+Console.WriteLine("|______\\__,_|___/\\__, |___/\\__,_| \\_/ \\___|");
+Console.WriteLine("                  __/ |                    ");
+Console.WriteLine("                 |___/                       \n");
